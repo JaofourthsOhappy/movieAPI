d2a11f7590697315ed4dbce8b12280cf7ee722d7
JaofourthsOhappy <=>
2023-03-09 13:54:03 +0700

    add MovieController

---
+++ src/main/java/dev/fourth/movies/MovieController.java
@@ -0,0 +1,22 @@
+package dev.fourth.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    // public String allmovies(){
+    //     return "All movies!!!";
+    // }
+
+    public ResponseEntity<String> allmovies(){
+        return new ResponseEntity<String>("All movies!!!" ,HttpStatus.OK);
+    }
+}
+ 
