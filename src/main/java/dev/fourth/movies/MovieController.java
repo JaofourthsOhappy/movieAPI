639b66b0e3109e6f32bd4327f3c6cfb6a620a239
JaofourthsOhappy <=>
2023-03-09 14:56:44 +0700

    add MovieRepos

--- src/main/java/dev/fourth/movies/MovieController.java
+++ src/main/java/dev/fourth/movies/MovieController.java
@@ -1,22 +1,27 @@
 package dev.fourth.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService moviesService;
     @GetMapping
     // public String allmovies(){
     //     return "All movies!!!";
     // }
 
-    public ResponseEntity<String> allmovies(){
-        return new ResponseEntity<String>("All movies!!!" ,HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(moviesService.allMovies(),HttpStatus.OK);
     }
 }
  
