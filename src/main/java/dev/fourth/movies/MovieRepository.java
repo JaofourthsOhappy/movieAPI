993b4d7457d2f3985b0b009b6988c18ef051d467
JaofourthsOhappy <=>
2023-03-12 15:50:29 +0700

    Add path id

--- src/main/java/dev/fourth/movies/MovieRepository.java
+++ src/main/java/dev/fourth/movies/MovieRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends MongoRepository<Movie,ObjectId>{
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
 
 }
     
--- src/main/java/dev/fourth/movies/MovieService.java
+++ src/main/java/dev/fourth/movies/MovieService.java
@@ -3,14 +3,20 @@ package dev.fourth.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
+import java.util.Optional;
+import org.bson.types.ObjectId;
 
 @Service
 public class MovieService {
+    
     @Autowired
     private MovieRepository moviesRepository;
 
     public List<Movie> allMovies(){
         return moviesRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return moviesRepository.findById(id);
     }
 }
